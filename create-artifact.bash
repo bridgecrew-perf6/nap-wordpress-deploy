5f66765f4f7b75ac9397a62466e1a9fc1d0b66cf
seubpong.mon <pjame.fb@gmail.com>
2022-02-28 07:31:38 +0700

    Refactored code

--- create-artifact.bash
+++ create-artifact.bash
@@ -6,17 +6,11 @@ APP="wp"
 SETTING_FILE="setting-${CI_COMMIT_BRANCH}.cfg"
 CFG_FILE=setting.cfg
 
-
-SA_KEY=sa-dev.json
-if [ "${CI_COMMIT_BRANCH}" == "production" ]; then
-    SA_KEY=sa-prod.json
-fi
-gcloud auth activate-service-account --key-file=${SA_KEY}
-
-
 cp ${SETTING_FILE} ${CFG_FILE}
-
 set -o allexport; source "${SETTING_FILE}"; set +o allexport
+
+
+gcloud auth activate-service-account --key-file=${SA_KEY}
 
 tar -cvf ${APP}.tar entry.bash docker-compose.yaml ${CFG_FILE} configs
 gzip ${APP}.tar
